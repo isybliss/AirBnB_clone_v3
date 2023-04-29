15baf50a15971ef70bab99aefd6a9bdf67a52b77
DT-GAMER <abakpad82@gmail.com>
2023-04-29 19:35:14 +0100

    update file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,40 @@
-#!/usr/bin/python
-"""flask"""
-from flask import flask, render_template, jsonify
-from model import storage
-from api.v1.views import app_views
-from os import getenv
-from flask_cors import CORS
+#!/usr/bin/python3
+"""
+API for AirBnB_clone_v3
+"""
 
-app = flask(__name__)
-app.register_blueprint(app_views, url_prefix="/api/v1")
-host = getenv('HBNB_API_HOST') if getenv('HBNB_API_HOST') else '0.0.0.0'
-port = getenv('HBNB_API_PORT') if getenv('HBNB_API_PORT') else 5000
-cors = CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
+import os
+from flask import Flask, jsonify, Response
+from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+app = Flask(__name__)
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_db(self):
-    """teardown"""
+def teardown(self):
+    """ handles teardown """
     storage.close()
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-    """404ed"""
-    return jsonify({"error": "Not found"}), 404
+    """ handles 404 errors """
+    status = {"error": "Not found"}
+    return jsonify(status), 404
 
 
 if __name__ == '__main__':
-    app.run(host=host, port=port, threaded=True)
+    try:
+        host = os.environ.get('HBNB_API_HOST')
+    except:
+        host = '0.0.0.0'
+
+    try:
+        port = os.environ.get('HBNB_API_PORT')
+    except:
+        port = '5000'
+
+    app.run(host=host, port=port)
